78265f6d059e7afea9897d83f940c1a3b4c6c059
Ibrahim Fawzy <62511760+fawzyibra@users.noreply.github.com>
2021-03-02 06:34:08 +0200

    finaal timer

--- pomodoro_app/lib/screens/Timer/timer.dart
+++ pomodoro_app/lib/screens/Timer/timer.dart
@@ -5,6 +5,7 @@ import 'package:pomodoro_app/screens/Timer/break_time.dart';
 import 'package:pomodoro_app/widgets/drawer.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:slide_countdown_clock/slide_countdown_clock.dart';
+import 'package:intl/intl.dart';
 
 class TimerScreen extends StatefulWidget {
   TimerScreen({Key key}) : super(key: key);
@@ -14,61 +15,72 @@ class TimerScreen extends StatefulWidget {
 }
 
 class _TimerScreenState extends State<TimerScreen> {
-  bool startispressed = true;
-  bool stopispressed = true;
-  bool resetispressed = true;
-  String stopTimetodisplay = "00:00:00";
-  var swatch = Stopwatch();
-  final dur = const Duration(seconds: 1);
+  int _seconds = 00;
+  int _minutes = 25;
+  Timer _timer;
 
-  void starttimer() {
-    Timer(dur, keepRunning);
+  var f = NumberFormat("00");
+
+  bool _canShowButton = true;
+  bool _canShowReset = false;
+  bool _canShowPause = false;
+  bool _canShowResume = false;
+
+  bool state = false;
+
+  void hideWidget() {
+    setState(() {
+      _canShowButton = !_canShowButton;
+      _canShowReset = !_canShowReset;
+      _canShowPause = !_canShowPause;
+    });
   }
 
-  void keepRunning() {
-    if (swatch.isRunning) {
-      starttimer();
+  void _restartTimer() {
+    if (_timer != null) {
+      _timer.cancel();
+      _seconds = 0;
+      _minutes = 25;
     }
-    setState(() {
-      stopTimetodisplay = swatch.elapsed.inHours.toString().padLeft(2, "0") +
-          ":" +
-          (swatch.elapsed.inMinutes % 60).toString().padLeft(2, "0") +
-          ":" +
-          (swatch.elapsed.inSeconds % 60).toString().padLeft(2, "0");
+  }
 
-      if (stopTimetodisplay == "00:25:00") {
-        swatch.stop();
-        stopTimetodisplay = "You finished the Work .. Press Break button";
-        stopispressed = true;
-      }
+  void _stopTimer() {
+    _timer.cancel();
+  }
+
+  void _toggleTimer(bool state) {
+    state = !state;
+    if (state) {
+      _resumeTimer();
+    } else {
+      _stopTimer();
+    }
+  }
+
+  void _startTimer() {
+    if (_seconds > 60) {
+      _minutes = (_seconds / 60).floor();
+      _seconds -= (_minutes * 60);
+    }
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_seconds > 0) {
+          _seconds--;
+        } else {
+          if (_minutes > 0) {
+            _seconds = 59;
+            _minutes--;
+          } else {
+            _timer.cancel();
+            print("Timer Complete");
+          }
+        }
+      });
     });
   }
 
-  void startStopwatch() {
-    setState(() {
-      stopispressed =
-          false; // already msh sha8ala f 5aletha heya el wa7eda ely tsht8l
-      startispressed = false; // 3shan y2fl el start button mysht8lsh
-    });
-    swatch.start();
-    starttimer();
-  }
-
-  void stopStopwatch() {
-    setState(() {
-      stopispressed = true;
-      resetispressed = false;
-    });
-    swatch.stop();
-  }
-
-  void resetStopwatch() {
-    setState(() {
-      startispressed = true;
-      resetispressed = true;
-    });
-    swatch.reset();
-    stopTimetodisplay = "00:00:00";
+  void _resumeTimer() {
+    _timer.cancel();
   }
 
   Widget build(BuildContext context) {
@@ -76,114 +88,117 @@ class _TimerScreenState extends State<TimerScreen> {
       child: Scaffold(
         drawer: MyDrawer(),
         appBar: GradientAppBar(
-          title: Text('Timer Page'),
+          title: Text('Pomodoro Timer'),
           backgroundColorStart: Colors.cyan,
           backgroundColorEnd: Colors.green,
         ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Your Work time is 25 min. and the timer will stop",
-                    style:
-                        TextStyle(fontSize: 10.0, fontWeight: FontWeight.w700),
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "${f.format(_minutes)} : ${f.format(_seconds)}",
+                  style: TextStyle(
+                    color: Colors.cyan[900],
+                    fontSize: 58,
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 6,
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    stopTimetodisplay,
-                    style:
-                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 4,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          RaisedButton(
-                            onPressed: stopispressed ? null : stopStopwatch,
-                            color: Colors.red,
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 40.0,
-                              vertical: 15.0,
-                            ),
-                            child: Text(
-                              "Stop",
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.white),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                !_canShowButton
+                    ? const SizedBox.shrink()
+                    : RaisedButton(
+                        onPressed: () {
+                          setState(() {
+                            _startTimer();
+                            hideWidget();
+                          });
+                        },
+                        color: Colors.green[400],
+                        shape:
+                            CircleBorder(side: BorderSide(color: Colors.green)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(40.0),
+                          child: Text(
+                            "Start",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
                             ),
                           ),
-                          RaisedButton(
-                            onPressed: resetispressed ? null : resetStopwatch,
-                            color: Colors.teal,
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 40.0,
-                              vertical: 15.0,
-                            ),
-                            child: Text(
-                              "Reset",
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.white),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          RaisedButton(
-                            onPressed: startispressed ? startStopwatch : null,
-                            color: Colors.green,
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 40.0,
-                              vertical: 15.0,
-                            ),
-                            child: Text(
-                              "Start",
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.white),
-                            ),
-                          ),
-                          RaisedButton(
-                            onPressed: () {
-                              Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Break()));
-                            },
-                            color: Colors.black,
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 40.0,
-                              vertical: 15.0,
-                            ),
-                            child: Text(
-                              "Break",
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.white),
-                            ),
-                          ),
-                        ],
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Visibility(
+                  visible: _canShowReset,
+                  child: RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        _restartTimer();
+                        hideWidget();
+                        _canShowButton = true;
+                      });
+                    },
+                    color: Colors.cyan[900],
+                    shape:
+                        CircleBorder(side: BorderSide(color: Colors.cyan[900])),
+                    child: Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Text(
+                        "Reset",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
+                Visibility(
+                  visible: _canShowPause,
+                  child: RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        state = !state;
+                        _toggleTimer(state);
+                        hideWidget();
+                      });
+                    },
+                    color: Colors.cyan,
+                    shape: CircleBorder(side: BorderSide(color: Colors.cyan)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Text(
+                        "Pause",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
