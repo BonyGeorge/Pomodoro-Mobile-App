87863e524710a185672f678725f521db22e83038
Ibrahim Fawzy <62511760+fawzyibra@users.noreply.github.com>
2021-03-07 15:35:23 +0200

    ptList

--- pomodoro_app/lib/screens/projects/ProjectTask.dart
+++ pomodoro_app/lib/screens/projects/ProjectTask.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
-import 'package:pomodoro_app/models/task.dart';
-import 'package:pomodoro_app/widgets/comp_list.dart';
+import 'package:pomodoro_app/models/project.dart';
+import 'package:pomodoro_app/widgets/pt_list.dart';
 import 'package:provider/provider.dart';
-import 'package:pomodoro_app/providers/tasks.dart';
+import 'package:pomodoro_app/providers/projects.dart';
 
 class AddPTask extends StatefulWidget {
   static const routeName = '/projectTask';
-  Task task;
+  Project pro;
 
   @override
   _AddPTaskState createState() => _AddPTaskState();
@@ -16,7 +16,7 @@ class AddPTask extends StatefulWidget {
 class _AddPTaskState extends State<AddPTask> {
   @override
   Widget build(BuildContext context) {
-    final tasks = Provider.of<TaskProvider>(context).compList;
+    final pro = Provider.of<ProjectProvider>(context).projectsTasks;
     return Scaffold(
       appBar: GradientAppBar(
         title: Text('Project Tasks'),
@@ -24,11 +24,11 @@ class _AddPTaskState extends State<AddPTask> {
         backgroundColorEnd: Colors.green,
         centerTitle: true,
       ),
-      body: tasks.length > 0
+      body: pro.length > 0
           ? ListView.builder(
-              itemCount: tasks.length,
+              itemCount: pro.length,
               itemBuilder: (context, index) {
-                return CompItem(tasks[index]);
+                return PTItem(pro[index]);
               },
             )
           : LayoutBuilder(
---
+++ pomodoro_app/lib/widgets/pt_list.dart
@@ -0,0 +1,78 @@
+// Widget responsible for displaying tasks in the form of a list.
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pomodoro_app/widgets/task_name.dart';
+import 'package:pomodoro_app/providers/projects.dart';
+import 'package:pomodoro_app/models/project.dart';
+
+class PTItem extends StatefulWidget {
+  final Project pt;
+  PTItem(this.pt);
+
+  @override
+  _PTItemState createState() => _PTItemState();
+}
+
+class _PTItemState extends State<PTItem> {
+  @override
+  Widget build(BuildContext context) {
+    void _deleteTask() {
+      setState(() {
+        Provider.of<ProjectProvider>(context, listen: false).fill(widget.pt.id);
+      });
+    }
+
+    return Dismissible(
+      key: ValueKey(widget.pt.id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (_) {
+        _deleteTask();
+      },
+      background: Container(
+        color: Theme.of(context).errorColor,
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Delete',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Lato',
+                fontSize: 10,
+              ),
+            ),
+            SizedBox(width: 5),
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+      child: Container(
+        height: 65,
+        child: Card(
+          elevation: 10,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  ItemText(
+                    widget.pt.isCompleted,
+                    widget.pt.title,
+                    widget.pt.dueDate,
+                    widget.pt.dueTime,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
